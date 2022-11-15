2b0673b0276c620b74105749640daacc367deac3
Ptriantafyll <ptriantafyll@gmail.com>
2022-11-15 23:23:08 +0200

    Changed mongodb connection

--- Api/app.py
+++ Api/app.py
@@ -1,16 +1,20 @@
+import os
 from flask import Flask
 from flask_restful import Resource, Api
 from pymongo import MongoClient
+from dotenv import load_dotenv
+
+load_dotenv() # load .env file
 
 app = Flask(__name__)
 api = Api(app)
 
-client = MongoClient("mongodb+srv://PanosTriantafyllopoulos:1234@poidb.gojkx.mongodb.net/?retryWrites=true&w=majority")
-db = client.test # test is the name of the db
+client = MongoClient("mongodb+srv://PanosTriantafyllopoulos:" + os.getenv("MONGODB_ATLAS_PW") + "@poidb.gojkx.mongodb.net/?retryWrites=true&w=majority")
+# db = client.test # test is the name of the db
 # users = db['users']
 # cursor = users.find({})
 # for user in cursor:
-#     print(user["username"])
+    # print(user["username"])
 
 class HelloWorld(Resource):
     def get(self):
