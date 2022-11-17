0e98181c06a176ff0493cb0d182d11ee7aeee34a
Panagiotis Triantafyllopoulos (Παναγιώτης Τριανταφυλλόπουλος) <ptriantafyll@gmail.com>
2022-11-17 11:02:19 +0200

    Refactored mongodb connection to a different file

---
+++ Api/mongoConnection.py
@@ -0,0 +1,6 @@
+# client = MongoClient("mongodb+srv://PanosTriantafyllopoulos:" + os.getenv("MONGODB_ATLAS_PW") + "@poidb.gojkx.mongodb.net/?retryWrites=true&w=majority")
+# db = client.test # test is the name of the db
+# users = db['users']
+# cursor = users.find({})
+# for user in cursor:
+    # print(user["username"])
