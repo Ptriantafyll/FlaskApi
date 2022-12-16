364234225c23b941f8f094274b6cd079db0171ca
Ptriantafyll <ptriantafyll@gmail.com>
2022-12-16 13:20:00 +0200

    Updated user model with id

--- Api/models/user.py
+++ Api/models/user.py
@@ -4,6 +4,9 @@ def validator():
         "$jsonSchema": {
             "bsonType": "object",
             "properties": {
+                "_id": {
+                    "bsonType": "objectId"
+                },
                 "links": {
                     "bsonType": "array",
                     "items": {
