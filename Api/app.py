7c21f7550e8ab0538bf044f00d2016da06ba362e
Panagiotis Triantafyllopoulos (Παναγιώτης Τριανταφυλλόπουλος) <ptriantafyll@gmail.com>
2022-12-16 11:08:23 +0200

    restructuring

--- Api/app.py
+++ Api/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
 
+from routes import user as user_routes
+
 
 app = Flask(__name__)
 api = Api(app)
@@ -9,22 +11,13 @@ class HelloWorld(Resource):
     def get(self):
         return {"message": "Hello world"}
 
-class KeepUser(Resource):
-    # todo: make post request to /newuser create a user in the database
-    def get(self):
-        return {"new": "user"}
-    def post(self):
-        # args = parser.parse_args()
-        # print(request.json)
-        # todo: here we should create the user
-        return {"post" : "request"}
 
 # parser = reqparse.RequestParser()
 # parser.add_argument("data", type=int, help="my test data") # parses 'data' argument (needs to be int)
+# args = parser.parse_args()
 
 # adding resources
 api.add_resource(HelloWorld, '/')
-api.add_resource(KeepUser, "/newuser")
 
 if __name__ == '__main__':
     app.run(debug=True)
