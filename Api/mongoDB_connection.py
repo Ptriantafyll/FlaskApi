190031fa9202a392a0ed2db9309197403c95ce16
Ptriantafyll <ptriantafyll@gmail.com>
2022-12-16 12:56:56 +0200

    Added mongodb connection file

---
+++ Api/mongoDB_connection.py
@@ -0,0 +1,10 @@
+import os
+from pymongo import MongoClient
+from dotenv import load_dotenv
+from models import user
+
+def connect_to_mongodb():
+    load_dotenv() # load .env file
+    client = MongoClient("mongodb+srv://PanosTriantafyllopoulos:" + os.getenv("MONGODB_ATLAS_PW") +"@url-rating.jdbbkmz.mongodb.net/?retryWrites=true&w=majority&authSource=admin")
+    global db
+    db = client['url-rating']
