fd9b2ffb9c386b48248415e9f371cd73b5f01688
Ptriantafyll <ptriantafyll@gmail.com>
2023-02-07 11:39:32 +0200

    removed soup.py file

--- Api/soup.py
+++
@@ -1,8 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-r = requests.get("https://en.wikipedia.org/wiki/2022_FIFA_World_Cup")
-soup = BeautifulSoup(r.text, "html.parser")
-
-for link in soup.find_all("a"):
-    print(link)
