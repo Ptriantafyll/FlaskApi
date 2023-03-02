b1cfa7aa540a107d3a612e43d83fa0eda806fe7a
Ptriantafyll <ptriantafyll@gmail.com>
2023-03-02 13:35:52 +0200

    selenium first code drop

--- Api/resources/user.py
+++ Api/resources/user.py
@@ -4,6 +4,9 @@ from flask_restful import Resource
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 class CreateUser(Resource):
     def post(self):
@@ -25,14 +28,41 @@ class NewRating(Resource):
 
 class GetRatings(Resource):
     def post(self, userId):
-        data = request.json
-        url = data["url"]
+        # data = request.json
+        # url = data["url"]
+
+        # r = requests.get(url)
+        # soup = BeautifulSoup(r.text, "html.parser")
+        # links_of_current_page = [urljoin(url, link.get('href')) for link in soup.find_all('a')]
+
+        # ratings = user_controller.get_ratings_for_user(
+        #     userId, links_of_current_page)
+
+        url = "https://en.wikipedia.org/wiki/FIFA_World_Cup"
 
         r = requests.get(url)
         soup = BeautifulSoup(r.text, "html.parser")
         links_of_current_page = [urljoin(url, link.get('href')) for link in soup.find_all('a')]
+        print("requests link count: " + str(len(links_of_current_page)))
 
+        
+
+        # driver = webdriver.Chrome('./chromedriver.exe')  # or other webdriver
+        # driver.get(url)
+        # links_of_current_page = [link.get_attribute('href') for link in driver.find_elements_by_tag_name('a')]
+        # driver.quit()
+        # print("requests link count: " + str(len(links_of_current_page)))
+
+        driver = webdriver.Chrome()
+        driver.maximize_window()
+        driver.get(url)
+        links_of_current_page = driver.find_elements(By.TAG_NAME, "a")
+        driver.quit()
+        print("SELENIUM link count: " + str(len(links_of_current_page)))
+        
+
+        
         ratings = user_controller.get_ratings_for_user(
             userId, links_of_current_page)
-
+        
         return {"user": userId, "ratings": ratings}, 200
