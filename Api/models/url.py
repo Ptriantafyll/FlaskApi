7fcc22673e10c7b68d0947b3857d5ab995b76fde
Ptriantafyll <ptriantafyll@gmail.com>
2023-06-27 10:49:19 +0300

    Added validator for url collection

--- Api/models/url.py
+++ Api/models/url.py
@@ -1,2 +1,19 @@
-import pymongo
-# TODO: make url model
+def validator():
+    return {
+        "$jsonSchema": {
+            "bsonType": "object",
+            "properties": {
+                "_id": {
+                    "bsonType": "objectId"
+                },
+                "url": {
+                    "bsonType": "string",
+                    "pattern": "^https?:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)$"
+                },
+                "text": {
+                    "bsonType": "string",
+                    "description": "text of the website",
+                }
+            }
+        }
+    }
