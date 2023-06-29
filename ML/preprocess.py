8562878c16854ac88206ec64361f56eac5890391
Ptriantafyll <ptriantafyll@gmail.com>
2023-06-29 10:07:05 +0300

    Continuing preprocessing

--- ML/preprocess.py
+++ ML/preprocess.py
@@ -3,6 +3,12 @@
 
 import pandas as pd
 import json
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
@@ -29,7 +35,7 @@ my_json_object = json.loads(my_string)
 # print(my_json_object[0]["url"])
 # print(my_json_object[0]["rating"])
 
-# todo: have selnium access the links text
+# ? have selnium access the links text
 chrome_options = Options()
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--headless')
@@ -38,15 +44,16 @@ driver = webdriver.Chrome(options=chrome_options)
 
 driver.maximize_window()
 
-url = my_json_object[0]["url"]
+url = my_json_object[1]["url"]
 driver.get(url)
 website_text = driver.find_element(By.XPATH, "/html/body").text
-# print(website_text)
+print(url)
+print(website_text)
 # print(type(website_text))
 
 driver.quit()
 
-# todo: Add url and text into mongodb to see the format - add the texts of all websites
+# todo add the texts of all websites to mongodb
 
 # mongoDB_connection.connect_to_mongodb()  # todo: maybe remove that later
 
@@ -58,11 +65,84 @@ driver.quit()
 #         "text": website_text
 #     }
 # )
-print(my_json_object)
+
+
+# print(my_json_object)
 # for user in my_json_object :
 #     print(user)
 
 
 # todo: feature extraction from words to vector?
+
+# nltk.download('punkt')
+# nltk.download('stopwords')
+# nltk.download('wordnet')
+
+print("making lowercase")
+lcsentence = website_text.lower()
+print(lcsentence)
+
+# print("tokenizing")
+# words = word_tokenize(lcsentence)  # ? words to tokens
+# print(words)
+
+print("reg exp tokenizing")
+tokenizer = RegexpTokenizer(r'\w+')  # ? remove punctuation
+words = tokenizer.tokenize(lcsentence)
+words = [word for word in words if word.isalpha()]  # ? remove numbers
+
+print("printing stop words")
+stop_words = set(stopwords.words('english'))
+print(stop_words)
+
+# ! will need that for greek websites
+# print("printing stop words greek")
+# stop_words = set(stopwords.words('greek'))
+# print(stop_words)
+
+print("removing stop words")
+filtered_words = [w for w in words if not w in stop_words]
+# todo: probably add greek stop word removal
+# print("before")
+# print(words)
+# print("after")
+print(filtered_words)
+
+print("stemming")
+ps = PorterStemmer()
+stemmed_words = []  # ! maybe make it set?
+for word in filtered_words:
+    stemmed_words.append(ps.stem(word))
+
+print(stemmed_words)
+
+
+print("lemmatizing")
+lemmatizer = WordNetLemmatizer()
+lemmatized_words = []  # ! maybe make it set?
+for word in filtered_words:
+    lemmatized_words.append(lemmatizer.lemmatize(word))
+
+print(lemmatized_words)
 # todo: create tf-idf vectors
 # todo: word embedding gensim and bert
+
+
+
+
+# import spacy
+
+# # Load the Greek language model in spacy
+# nlp = spacy.load("el_core_news_sm")
+
+# # Text to be lemmatized
+# text = "Τα παιδιά παίζουν στο πάρκο και διασκεδάζουν."
+
+# # Tokenize the text
+# doc = nlp(text)
+
+# # Lemmatize each token
+# lemmas = [token.lemma_ for token in doc]
+
+# # Print the lemmas
+# print(lemmas)
