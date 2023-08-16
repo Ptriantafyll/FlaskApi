cc26ee1423302095352b20497bb509d636a13efb
Ptriantafyll <ptriantafyll@gmail.com>
2023-08-16 12:53:01 +0300

    Added gensim word2vec model for word embeddings

--- ML/gensim_word_embeddings.py
+++ ML/gensim_word_embeddings.py
@@ -3,31 +3,50 @@
 from gensim.models import Word2Vec
 import nltk
 from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+import json
+import logging
 
 
 def preprocess_document(doc):
+    stop_words = set(stopwords.words('english'))
+    greek_stop_words = set(stopwords.words('greek'))
+
     tokens = word_tokenize(doc.lower())  # ?Tokenize and lowercase
-    # ?Remove punctuation
-    filtered_tokens = [token for token in tokens if token.isalpha()]
+    # ?Remove punctuation and stopwords
+    filtered_tokens = [token for token in tokens if token.isalpha(
+    ) and token not in stop_words and token not in greek_stop_words]
+
     return filtered_tokens
 
 
-# ? gensim word embeddings
-# todo: Take docs from mongo db
-docs = [
-    "apple pear banana",
-    "apple mango"
-]
-documents = [preprocess_document(doc) for doc in docs]
-print(documents)
+logging.basicConfig(
+    format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
+# ? gensim word embeddings
+url_file = open(
+    r"C:\Users\ptria\source\repos\FlaskApi\Web-Scraping\json\urls.json", encoding="utf8")
+urls = json.load(url_file)
+
+documents = [preprocess_document(url['text']) for url in urls]
+
+# for i in range(2):
+# print(documents[i])
 
 # Train the Word2Vec model
 model = Word2Vec(sentences=documents, vector_size=100,
-                 window=5, min_count=1, workers=4)
+                 window=10, min_count=1, workers=4)
+model.train(documents, total_examples=len(documents), epochs=10)
+
 
 # Save the model to a file (optional)
-# model.save("word2vec_model")
+# model.save(r"C:\Users\ptria\source\repos\FlaskApi\ML\word2vec_model")
 
 # Load the model from a file (if needed)
-# model = Word2Vec.load("word2vec_model")
+# model = Word2Vec.load(
+#     r"C:\Users\ptria\source\repos\FlaskApi\ML\word2vec_model")
+
+# ? example most similar words
+# w = "μπύρα"
+# print("most similar to: ", w)
+# print(model.wv.most_similar(positive=w, topn=10))
