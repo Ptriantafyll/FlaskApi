29963a79b7b8b7b7ecc059731a13df1a7f74229e
Ptriantafyll <ptriantafyll@gmail.com>
2023-08-24 13:20:56 +0300

    Fixed wrong path

--- ML/word-embeddings/fasttext_pretrained_embeddings.py
+++ ML/word-embeddings/fasttext_pretrained_embeddings.py
@@ -3,7 +3,7 @@ import fasttext.util
 
 
 ft = fasttext.load_model(
-    r"C:\Users\ptria\source\repos\FlaskApi\ML\cc.el.300.bin")
+    r"C:\Users\ptria\source\repos\FlaskApi\ML\word-embeddings\cc.el.300.bin")
 # print(ft.get_dimension())
 # fasttext.util.reduce_model(ft, 100)
 # print(ft.get_dimension())
